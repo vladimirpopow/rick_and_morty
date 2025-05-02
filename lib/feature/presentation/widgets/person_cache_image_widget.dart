fdc085cc05ee7114fefe1f958b11a0f2894341cf
Vladimir <satanpower1984@gmail.com>
2025-05-02 18:46:39 +0400

    add cache image widget

---
+++ lib/feature/presentation/widgets/person_cache_image_widget.dart
@@ -0,0 +1,44 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PersonCacheImage extends StatelessWidget {
+  final String imageUrl;
+  final double width;
+  final double height;
+
+  const PersonCacheImage(
+      {super.key,
+      required this.imageUrl,
+      required this.width,
+      required this.height});
+
+  Widget _imageWidget(ImageProvider imageProvider) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8), bottomLeft: Radius.circular(8)),
+          image: DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl ?? '',
+      width: width,
+      height: height,
+      imageBuilder: (context, imageProvider) {
+        return _imageWidget(imageProvider);
+      },
+      placeholder: ((context, url) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }),
+      errorWidget: (context, url, error) {
+        return _imageWidget(AssetImage('assets/image/noimage.jpg'));
+      },
+    );
+  }
+}
